b62dbb7e26726ee19f4e0423653dc66a4274db0f
Cao Van Nhat <nhatcv.cao@gmail.com>
2021-10-18 22:17:29 +0700

    [ADD] Initial commit for om_mobile_shop

---
+++ om_mobile_shop/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Mobile Shop Management',
+    'version': '14.0.2.1.0',
+    'summary': 'Odoo 14 Development',
+    'sequence': -100,
+    'description': """Odoo 14 Development""",
+    'category': 'Productivity',
+    'author': 'Group 8',
+    'maintainer': 'Group 8',
+    'website': 'https://www.due.udn.vn',
+    'license': 'LGPL-3',
+    'depends': [],
+    'data': [
+    ],
+    'demo': [],
+    'qweb': [],
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
